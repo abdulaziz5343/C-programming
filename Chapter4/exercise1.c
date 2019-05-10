d8adc6881ee934c50919ac790919bf902ef7756e
abdulaziz5343 <41600587+abdulaziz5343@users.noreply.github.com>
2019-05-10 11:37:44 +0900

    Reversal two-digit number

---
+++ Chapter4/exercise1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main(void) {
+
+	int num, frac1, frac2;
+	printf("Enter a two-digit number: ");
+	scanf_s("%d", &num);
+
+	frac1 = num / 10;
+	frac2 = num % 10;
+	printf("The reversal is %d%d\n", frac2, frac1);
+	return 0;
+
+}
