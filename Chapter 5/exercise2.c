51698cddaf8a4b5aa25e2796047e130519af8c35
abdulaziz5343 <41600587+abdulaziz5343@users.noreply.github.com>
2019-05-13 00:27:42 +0900

    Convert 24-hour time to 12-hour form

--- Chapter 5/exercise2.c
+++ Chapter 5/exercise2.c
@@ -1,15 +1,19 @@
 #include <stdio.h>
-int main(void) {
-	int hh, mm;
-	printf("Enter a 24-hour time: ");
-	scanf_s("%d:%d", &hh, &mm);
-	
-	if (hh >= 00 && hh <= 11)
-		printf("Equivalent 12-hour time: %d:%d AM\n", hh, mm);
-	else if (hh==12)
-		printf("Equivalent 12-hour time: %d:%d PM\n", hh, mm);
-	else
-		printf("Equivalent 12-hour time: %d:%d PM\n", hh-12, mm);
-	return 0;
 
-}
+int main(void)
+{
+	int data_hh, data_mm;
+
+	printf("Enter a 24-hour time: ");
+	scanf_s("%2d:%2d", &data_hh, &data_mm);
+
+	if ((data_hh >= 0 && data_hh < 12) && (data_mm >= 0 && data_mm <= 59))
+		printf("Equivalent 12-hour time: %d:%d AM\n", data_hh, data_mm);
+	else if ((data_hh == 12) && (data_mm >= 0 && data_mm <= 59))
+		printf("Equivalent 12-hour time: %d:%d PM\n", data_hh, data_mm);
+	else if ((data_hh > 12 && data_hh <= 23) && (data_mm >= 0 && data_mm <= 59))
+		printf("Equivalent 12-hour time: %d:%d PM\n", data_hh - 12, data_mm);
+	else
+		printf("-----Invalid data-----\n\n");
+
+	return 0;
