c28c169ffce9b2f0d3f21d49981f31864d5fcf1a
abdulaziz5343 <41600587+abdulaziz5343@users.noreply.github.com>
2019-05-15 01:59:47 +0900

    Deal

---
+++ Chapter08/deal.c
@@ -0,0 +1,33 @@
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define NUM_SUITS 4
+#define NUM_RANKS 14
+
+int main(void) {
+	bool in_hand[NUM_SUITS][NUM_RANKS] = { false };
+	int num_cards, rank, suit;
+	const char rank_code[] = { '2', '3', '4', '5','6','7','8',
+							'9','t', 'j','q', 'k','a' };
+	const char suit_code[] = { 'c', 'd','h','s' };
+
+	srand((unsigned)time(NULL));
+	printf("Enter number of cards in hand: ");
+	scanf_s("%d", &num_cards);
+
+	printf("Your hand: ");
+	while (num_cards > 0) {
+		suit = rand() % NUM_SUITS;  /*pick a random suit*/
+		rank = rand() % NUM_RANKS;  /*pick a random rank*/
+		if (!in_hand[suit][rank]) {
+			in_hand[suit][rank] = true;
+			num_cards--;
+			printf(" %c%c", rank_code[rank], suit_code[suit]);
+		}
+	}
+	printf("\n");
+
+	return 0; 
+}
