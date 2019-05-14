4335361336d0def7dd159bc900e84c07db6dc91c
abdulaziz5343 <41600587+abdulaziz5343@users.noreply.github.com>
2019-05-14 17:03:27 +0900

    sizeof() function

---
+++ Chapter07/exercise06.c
@@ -0,0 +1,17 @@
+/* Write a program that prints the values of sizeof(int), sizeof(short),
+ * sizeof(long), sizeof(float), sizeof(double) and sizeof(long double).
+ */
+#include <stdio.h>
+
+int main(void)
+{
+	printf("\nSize of:\n");
+	printf("int - %ld\n", sizeof(int));
+	printf("short - %ld\n", sizeof(short));
+	printf("long - %ld\n", sizeof(long));
+	printf("float - %ld\n", sizeof(float));
+	printf("double - %ld\n", sizeof(double));
+	printf("long double - %ld\n\n", sizeof(long double));
+
+	return 0;
+}
